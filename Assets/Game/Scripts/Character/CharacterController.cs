33080c3cd5809676881c918af84865d812620ef6
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2023-04-04 17:13:35 -0400

    Basic Physics

--- Assets/Game/Scripts/Character/CharacterController.cs
+++ Assets/Game/Scripts/Character/CharacterController.cs
@@ -7,10 +7,19 @@ namespace Game.Character
 {
     public class CharacterController : MonoBehaviour
     {
+        // References
+        private new Rigidbody2D rigidbody;
+
+
+        private void Awake() 
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
+
         public void Move(InputAction.CallbackContext context) 
         {
             Vector2 input = context.ReadValue<Vector2>();
-            transform.position += (Vector3) input;
+            rigidbody.AddForce(input * 100);
         }
     }
 }
