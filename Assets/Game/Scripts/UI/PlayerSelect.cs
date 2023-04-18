898d2783b768126a1a36df106a8f5412cbd5cf3a
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2023-04-18 15:07:33 -0400

    Preview Prefab Functions

--- Assets/Game/Scripts/UI/PlayerSelect.cs
+++ Assets/Game/Scripts/UI/PlayerSelect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace Game.UI
 {
@@ -10,19 +11,48 @@ namespace Game.UI
     public class PlayerSelect : MonoBehaviour
     {
         // Inspector
-        [Header("Previews")]
-        [SerializeField] private GameObject[] previewPrefabs; 
-        [SerializeField] private Transform previewSpawnPoint;
+        [Header("Text")]
+        [SerializeField] private TMP_Text playerText;
+        [SerializeField] private TMP_Text statusText;
 
+        [Header("Previews")]
+        [SerializeField] private Transform previewSpawnPoint;
+        [SerializeField] private GameObject[] previewPrefabs;
+
+        // Internal
+        private Transform preview;
+
+
+        /// <summary>
+        /// Activates the Player Select according to the given player number.
+        /// </summary>
+        /// <param name="player">The number of player activating the player select.</param>
+        public void ActivatePlayer(int player)
+        {
+            playerText.SetText($"Player {player}");
+            statusText.SetText("Connected");
+            preview = SpawnPreviewPrefab(player);
+        }
+
+        /// <summary>
+        /// Deactivates the Player Select.
+        /// </summary>
+        public void DeactivatePlayer()
+        {
+            playerText.SetText("No Player");
+            statusText.SetText("Press Any Button");
+            Destroy(preview.gameObject);
+        }
 
         /// <summary>
         /// Spawns the preview prefab corresponding to the specified index.
         /// </summary>
         /// <param name="index">Designates which preview prefab to spawn.</param>
-        public void SpawnPreviewPrefab(int index)
+        public Transform SpawnPreviewPrefab(int index)
         {
             GameObject previewPrefabObj = Instantiate(previewPrefabs[index], Vector3.zero, Quaternion.identity, previewSpawnPoint);
             previewPrefabObj.transform.localPosition = Vector3.zero;
+            return previewPrefabObj.transform;
         }
     }
 }
